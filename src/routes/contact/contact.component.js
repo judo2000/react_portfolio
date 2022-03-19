5104bccb718e402c586bd0e7f5dbf98f5c9ecb51
Stephen Moore <judo2000@comcast.net>
2022-03-18 22:28:52 -0600

    Install emailjs to send customized email from my contact form

--- src/routes/contact/contact.component.js
+++ src/routes/contact/contact.component.js
@@ -1,4 +1,58 @@
+import { useState } from "react";
+import emailjs from "emailjs-com";
+
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = e.target;
+
+    // Ternary statement that will call either setFirstName or setLastName based on what field the user is typing in
+    if (name === "name") {
+      return setName(value);
+    } else if (name === "email") {
+      return setEmail(value);
+    } else if (name === "subject") {
+      return setSubject(value);
+    } else if (name === "message") {
+      return setMessage(value);
+    }
+  };
+
+  const handleBlur = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = e.target;
+
+    if (value === "") {
+      alert(`${name} cannot be blank`);
+    }
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_50q7t4j",
+        "template_0a3rkwk",
+        e.target,
+        "Ks2sBCa5CIPhtiwHp"
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+
+    alert("Your email has been sent to Stephen and you have been cc'd. ");
+    setName("");
+    setEmail("");
+    setSubject("");
+    setMessage("");
+  };
+
   return (
     <section className="contact_me sect_mt4">
       <div className="container">
@@ -13,57 +67,54 @@ const Contact = () => {
               <div className="row">
                 <div className="col-sm-12 col-md-6">
                   <div className="title_small pb-2">Send Me a Message:</div>
-                  <form
-                    action="mailto:judo2000@comcast.net"
-                    method="post"
-                    enctype="text/plain"
-                  >
+                  <form onSubmit={sendEmail} method="post" encType="text/plain">
                     <div className="mb-3">
-                      <label for="name" className="form-label">
-                        Name
-                      </label>
+                      <label className="form-label">Name</label>
                       <input
                         type="text"
                         className="form-control"
-                        id="name"
+                        value={name}
                         name="name"
                         placeholder="Name"
+                        required
+                        onBlur={handleBlur}
+                        onChange={handleInputChange}
                       />
                     </div>
                     <div className="mb-3">
-                      <label for="email" className="form-label">
-                        Email address
-                      </label>
+                      <label className="form-label">Email address</label>
                       <input
                         type="email"
                         className="form-control"
-                        id="email"
+                        value={email}
                         name="email"
                         placeholder="Email"
                         aria-describedby="emailHelp"
+                        onBlur={handleBlur}
+                        onChange={handleInputChange}
                       />
                     </div>
                     <div className="mb-3">
-                      <label for="subject" className="form-label">
-                        Subject
-                      </label>
+                      <label className="form-label">Subject</label>
                       <input
                         type="text"
                         className="form-control"
                         name="subject"
-                        id="subject"
+                        value={subject}
                         placeholder="Subject"
+                        onBlur={handleBlur}
+                        onChange={handleInputChange}
                       />
                     </div>
                     <div className="mb-3">
-                      <label for="message" className="form-label">
-                        Message
-                      </label>
+                      <label className="form-label">Message</label>
                       <textarea
-                        id="message"
+                        value={message}
                         className="form-control"
                         name="message"
                         placeholder="Your Message"
+                        onBlur={handleBlur}
+                        onChange={handleInputChange}
                       ></textarea>
                     </div>
                     <button type="send" className="btn btn-primary">
--- src/routes/navigation/navigation.component.js
+++ src/routes/navigation/navigation.component.js
@@ -8,10 +8,10 @@ import "./navigation.styles.css";
 const Navigation = () => {
   return (
     <Fragment>
-      <Nav class="navbar navbar-expand-md nav_style sticky-top">
-        <Container class="container">
+      <Nav className="navbar navbar-expand-md nav_style sticky-top">
+        <Container className="container">
           <button
-            class="navbar-toggler navbar-dark"
+            className="navbar-toggler navbar-dark"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -19,13 +19,9 @@ const Navigation = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon white-text"></span>
+            <span className="navbar-toggler-icon white-text"></span>
           </button>
-          <Nav
-            className="me-auto"
-            class="collapse navbar-collapse"
-            id="navbarSupportedContent"
-          >
+          <Nav className="collapse navbar-collapse" id="navbarSupportedContent">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/work">My Work</Nav.Link>
             <Nav.Link href="/contact">Contact Me</Nav.Link>
