7a4b11cdaf641ad0f9a056d4971ec7737bcceaa7
Stephen Moore <judo2000@comcast.net>
2022-12-13 13:40:11 -0700

    Remove projects housed on heroku until I can get them working somewhere else

--- src/routes/myWork/myWork.component.js
+++ src/routes/myWork/myWork.component.js
@@ -1,6 +1,6 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 
-import "./myWork.styles.css";
+import './myWork.styles.css';
 
 const MyWork = () => {
   return (
@@ -16,105 +16,6 @@ const MyWork = () => {
           <div className="row">
             <div className="col-12">
               <div className="row justify-content-center">
-                <div className="card col-sm-12 col-md-4 mx-md-4 my-4 py-1">
-                  <div className="work_box">
-                    <a
-                      href="https://judo2000-project-mgnt.herokuapp.com/"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <div className="work_img_box">
-                        <img
-                          src="/img/project_management.png"
-                          className="card-img-top img-fluid work_img"
-                          alt="Project Management System App"
-                        />
-                      </div>
-                    </a>
-                    <div className="card-body">
-                      <span className="title_small">
-                        Project Managment System
-                      </span>
-                      <br />
-                      <span className="title_small_sub">
-                        Expressjs, MySQLSequelize, Handlebars, CSS/Bootstrap,
-                        JavaScript,
-                      </span>
-                      <p>
-                        A full stack project management system that allows uses
-                        to create projects, add and remove tasks from the
-                        project and make tasks as complete.
-                      </p>
-                      <p>
-                        <a
-                          href="https://github.com/judo2000/proManagementSyst"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          Github Repository
-                        </a>
-                      </p>
-                      <p>
-                        <a
-                          href="https://judo2000-project-mgnt.herokuapp.com/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          Live Project
-                        </a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="card col-sm-12 col-md-4 mx-md-4 my-4 py-1">
-                  <div className="work_box">
-                    <a
-                      href="https://judo2000-gigabytes.herokuapp.com/"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <div className="work_img_box">
-                        <img
-                          src="/img/gigabytes.png"
-                          className="card-img-top img-fluid work_img"
-                          alt="Project Management System App"
-                        />
-                      </div>
-                    </a>
-                    <div className="card-body">
-                      <span className="title_small">Gigabytes Tech Blog</span>
-                      <br />
-                      <span className="title_small_sub">
-                        Expressjs, MySQLSequelize, Handlebars, CSS/Bootstrap,
-                        JavaScript,
-                      </span>
-                      <p>
-                        About Ful stack CMS -styled tech blog for developers to
-                        post articles about technology. Users can edit and
-                        delete their own articles as well as comment on other
-                        users' posts.
-                      </p>
-                      <p>
-                        <a
-                          href="https://github.com/judo2000/GigaBytes_tech_blog"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          Github Repository
-                        </a>
-                      </p>
-                      <p>
-                        <a
-                          href="http://judo2000-gigabytes.herokuapp.com/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          Live Project
-                        </a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
                 <div className="card col-sm-12 col-md-4 mx-md-4 my-4 py-1">
                   <div className="work_box">
                     <a
@@ -137,7 +38,7 @@ const MyWork = () => {
                         HTML, Bootstrap, JavaScript, jQuery
                       </span>
                       <p>
-                        An app allows users to view a 5-day forecast using the
+                        This app allows users to view a 5-day forecast using the
                         OpenWeatherMap api. The app also uses local storage to
                         save searched cities.
                       </p>
@@ -277,7 +178,7 @@ const MyWork = () => {
                         HTML, CSS/Bootstrap, JavaScript, jQuery
                       </span>
                       <p>
-                        {" "}
+                        {' '}
                         A JavaScript app that uses the moments.js API to display
                         the hours of the current day and uses local storage to
                         save a user's daily schedule.
---
+++ src/routes/myWork/temp.js
@@ -0,0 +1,99 @@
+// <div className="card col-sm-12 col-md-4 mx-md-4 my-4 py-1">
+//                   <div className="work_box">
+//                     <a
+//                       href="https://judo2000-project-mgnt.herokuapp.com/"
+//                       target="_blank"
+//                       rel="noreferrer"
+//                     >
+//                       <div className="work_img_box">
+//                         <img
+//                           src="/img/project_management.png"
+//                           className="card-img-top img-fluid work_img"
+//                           alt="Project Management System App"
+//                         />
+//                       </div>
+//                     </a>
+//                     <div className="card-body">
+//                       <span className="title_small">
+//                         Project Managment System
+//                       </span>
+//                       <br />
+//                       <span className="title_small_sub">
+//                         Expressjs, MySQLSequelize, Handlebars, CSS/Bootstrap,
+//                         JavaScript,
+//                       </span>
+//                       <p>
+//                         A full stack project management system that allows uses
+//                         to create projects, add and remove tasks from the
+//                         project and make tasks as complete.
+//                       </p>
+//                       <p>
+//                         <a
+//                           href="https://github.com/judo2000/proManagementSyst"
+//                           target="_blank"
+//                           rel="noreferrer"
+//                         >
+//                           Github Repository
+//                         </a>
+//                       </p>
+//                       <p>
+//                         <a
+//                           href="https://judo2000-project-mgnt.herokuapp.com/"
+//                           target="_blank"
+//                           rel="noreferrer"
+//                         >
+//                           Live Project
+//                         </a>
+//                       </p>
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <div className="card col-sm-12 col-md-4 mx-md-4 my-4 py-1">
+//                   <div className="work_box">
+//                     <a
+//                       href="https://judo2000-gigabytes.herokuapp.com/"
+//                       target="_blank"
+//                       rel="noreferrer"
+//                     >
+//                       <div className="work_img_box">
+//                         <img
+//                           src="/img/gigabytes.png"
+//                           className="card-img-top img-fluid work_img"
+//                           alt="Project Management System App"
+//                         />
+//                       </div>
+//                     </a>
+//                     <div className="card-body">
+//                       <span className="title_small">Gigabytes Tech Blog</span>
+//                       <br />
+//                       <span className="title_small_sub">
+//                         Expressjs, MySQLSequelize, Handlebars, CSS/Bootstrap,
+//                         JavaScript,
+//                       </span>
+//                       <p>
+//                         About Ful stack CMS -styled tech blog for developers to
+//                         post articles about technology. Users can edit and
+//                         delete their own articles as well as comment on other
+//                         users' posts.
+//                       </p>
+//                       <p>
+//                         <a
+//                           href="https://github.com/judo2000/GigaBytes_tech_blog"
+//                           target="_blank"
+//                           rel="noreferrer"
+//                         >
+//                           Github Repository
+//                         </a>
+//                       </p>
+//                       <p>
+//                         <a
+//                           href="http://judo2000-gigabytes.herokuapp.com/"
+//                           target="_blank"
+//                           rel="noreferrer"
+//                         >
+//                           Live Project
+//                         </a>
+//                       </p>
+//                     </div>
+//                   </div>
+//                 </div>
